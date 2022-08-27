7d1de2b6812435b5e7e642dbd647f84680ee3b9f
Aleksandra Polkowska <a.z.polkowska@gmail.com>
2022-08-27 09:15:41 +0200

    inicjalizacja Spring Boot App

---
+++ src/main/java/pl/cyber/trainees/demo/DemoApplication.java
@@ -0,0 +1,11 @@
+package pl.cyber.trainees.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // adnotacja ktorej zadaniem jest uurchomienie aplikacji springowej
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
